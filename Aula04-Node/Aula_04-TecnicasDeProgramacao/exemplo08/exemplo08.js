434a6f6f5e4bc5fcf8ece780cd8103d0010fcac5
Brunno Nascimento <brunnonascimento011@gmail.com>
2023-03-13 10:32:58 -0300

    alterações

---
+++ Aula04-Node/Aula_04-TecnicasDeProgramacao/exemplo08/exemplo08.js
@@ -0,0 +1,27 @@
+
+/*
+var nome = "Brunno"
+
+if (nome == "Brunno"){
+    var idade = 39 //var escopo global
+    //let idade = 39 //escopo local
+}
+
+console.log(idade)
+*/
+/*
+var nome = "Brunno"
+var nome = true
+console.log(nome)
+*/
+
+/*
+let nome = "Brunno"
+//let nome = true
+nome = "Joao"
+console.log(nome)
+*/
+
+const nome = "Joao"
+nome = "Paulo"
+console.log(nome)
---
+++ Aula04-Node/Aula_04-TecnicasDeProgramacao/exemplo11/script.js
@@ -0,0 +1,23 @@
+function coletarDados(){
+    document.getElementById("peso")
+    document.getElementById("altura")
+}
+
+function calcularIMC(peso, altura){
+    //IMC = Peso / (altura * altura)
+    altura = altura/100
+    let imc = peso / altura*2
+    function coletarDados()
+    function classificarIMC()
+}
+
+function classificarIMC(imc){
+    /*
+        IMC MENOR OU IGUAL A 18.5 = DESNUTRIÇÃO
+        IMC MAIOR QUE 18.5 E MENOR OU IGUAL A 25 = PESO NORMAL
+        IMC MAIOR QUE 25 E MENOR OU IGUAL A 30 = SOBREPESO
+        IMC MAIOR QUE 30 E MENOR OU IGUAL A 35 = OBESIDADE I
+        IMC MAIOR QUE 35 E MENOR OU IGUAL A 40 = OBESIDADE II
+        IMC MAIOR QUE 40 = OBESIDADE III
+    */
+}
