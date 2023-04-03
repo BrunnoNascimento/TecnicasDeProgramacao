6f5a62f227cbb0da9fe08f7eaf99d8337a0606fd
Brunno Nascimento <brunnonascimento011@gmail.com>
2023-04-03 09:41:55 -0300

    local host

---
+++ Aula07-Node/app.js
@@ -0,0 +1,15 @@
+//fazer a importação do express
+const express = require ('express')
+
+//Rotas (quando o seu servidor for acessado, pra onde ele vai???)
+const router = express.Router();
+router.get('/',(req,res)=>{
+    res.send('Olá Mundo!!!');
+});
+
+//configurações básicas do aplicativo
+const app = express();
+app.use('/',router);//foi passado apenas 1 rota, pois criamos apenas 1
+
+module.exports = app;
+
